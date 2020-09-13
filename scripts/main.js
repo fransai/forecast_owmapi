c121a55aa9bd1cfddec9df49487d7a702f76d1e4
Fran <38441102+fransai@users.noreply.github.com>
2020-09-13 19:00:17 +0100

    If Geolocation is denied
    Added logic to update the inner html content of a certain #id if the user denies the use of geolocation

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,16 @@ $(document).ready(function () {
   const apiKey = "d969704309dc87d01c3d23aab084f680";
 
   function lat_long(position) {
-    currLat = position.coords.latitude;
-    console.log(`Latitude : ${currLat}`);
-    currLong = position.coords.longitude;
-    console.log(`Longitude : ${currLong}`);
 
+    //add an if statement to make the below p1 work and if that doesnt work, make p2 work with some "Location Denied Messages"
+
+    var currLat = String(position.coords.latitude);
+    console.log(`Lattitude : ${currLat}`);
+    //console.log(typeof(currLat));
+    var currLong = String(position.coords.longitude);
+    console.log(`Longitude : ${currLong}`);
+    //console.log(typeof(currLat));
+    
     var xhr = new XMLHttpRequest();
     var weather;
 
@@ -16,29 +21,34 @@ $(document).ready(function () {
       }
     };
 
-  xhr.open("GET", "https://api.openweathermap.org/data/2.5/forecast?lat="+currLat+"&lon="+currLong+"&appid="+apiKey);
-  xhr.send();
+    xhr.open("GET", "https://api.openweathermap.org/data/2.5/forecast?lat="+currLat+"&lon="+currLong+"&appid="+apiKey);
+    xhr.send();
 
-  function setWeather(jsonWeather) {
-    weather = jsonWeather;
-    console.log(jsonWeather);
-    $("#city-name").html((jsonWeather.city.country) + ", " + (jsonWeather.city.name));
-    $("#population").html(jsonWeather.city.population);
-    var sunrise = jsonWeather.city.sunrise;
-    var hum_sunrise = new Date(1000*sunrise)
-    $("#sunrise").html(hum_sunrise);
-    var sunset = jsonWeather.city.sunset;
-    var hum_sunset = new Date(1000*sunset)
-    $("#sunset").html(hum_sunset);
+    function setWeather(jsonWeather) {
+      weather = jsonWeather;
+      console.log(jsonWeather);
+      $("#city-name").html((jsonWeather.city.country) + ", " + (jsonWeather.city.name));
+      $("#population").html(jsonWeather.city.population);
+      var sunrise = jsonWeather.city.sunrise;
+      var hum_sunrise = new Date(1000*sunrise)
+      $("#sunrise").html(hum_sunrise);
+      var sunset = jsonWeather.city.sunset;
+      var hum_sunset = new Date(1000*sunset)
+      $("#sunset").html(hum_sunset);
 
     }
-  
-    
-
   }
 
   navigator.geolocation.getCurrentPosition(lat_long);
 
-  
-  
+  var ultLoc = document.getElementById("city-name").innerText;
+      if (ultLoc == ""){
+        $("#city-name").html("Location N/A");
+        $("#population").html("Population N/A");
+        $("#sunrise").html("Sunrise N/A");
+        $("#sunset").html("Sunset N/A");
+      } else {
+      console.log("Woah")
+      }
+
 });
