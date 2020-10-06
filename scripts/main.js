69077f5bc148b2302914b847408e6b103a55e1fe
Fran <38441102+fransai@users.noreply.github.com>
2020-10-06 23:28:27 +0100

    Icons, additional weather attrs
    Added logic to retrieve icons based on weather conditions and added additional weather attrs (Temp, Tempmin, Tempmax, Time)

--- scripts/main.js
+++ scripts/main.js
@@ -11,17 +11,17 @@ $(document).ready(function () {
     var currLong = String(position.coords.longitude);
     console.log(`Longitude : ${currLong}`);
     //console.log(typeof(currLat));
-    
+
     var xhr = new XMLHttpRequest();
     var weather;
 
     xhr.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
-          setWeather(JSON.parse(this.responseText));
+        setWeather(JSON.parse(this.responseText));
       }
     };
 
-    xhr.open("GET", "https://api.openweathermap.org/data/2.5/forecast?lat="+currLat+"&lon="+currLong+"&units=metric"+"&appid="+apiKey);
+    xhr.open("GET", "https://api.openweathermap.org/data/2.5/forecast?lat=" + currLat + "&lon=" + currLong + "&units=metric" + "&appid=" + apiKey);
     xhr.send();
 
     function setWeather(jsonWeather) {
@@ -29,12 +29,13 @@ $(document).ready(function () {
       console.log(jsonWeather);
       $("#city-name").html((jsonWeather.city.country) + ", " + (jsonWeather.city.name));
       var sunrise = jsonWeather.city.sunrise;
-      var hum_sunrise = new Date(1000*sunrise)
+      var hum_sunrise = new Date(1000 * sunrise)
       $("#sunrise").html(hum_sunrise);
       var sunset = jsonWeather.city.sunset;
-      var hum_sunset = new Date(1000*sunset)
+      var hum_sunset = new Date(1000 * sunset)
       $("#sunset").html(hum_sunset);
-      $("#clouds").html((jsonWeather.list[0].clouds.all) + "% - " + "\""+ (jsonWeather.list[0].weather[0].description) + "\"");
+      $("#clouds").html((jsonWeather.list[0].clouds.all) + "% - " + "\"" + (jsonWeather.list[0].weather[0].description) + "\"");
+      $("#cloudsPiccurrent").attr("src", `http://openweathermap.org/img/wn/${(jsonWeather.list[0].weather[0].icon)}@2x.png`);
       $("#feelsLike").html((jsonWeather.list[0].main.feels_like) + "&deg;C");
 
     }
@@ -43,61 +44,60 @@ $(document).ready(function () {
   navigator.geolocation.getCurrentPosition(lat_long);
 
   var ultLoc = document.getElementById("city-name").innerText;
-      if (ultLoc == ""){
-        $("#city-name").html("Location Request Denied");
-        $("#sunrise").html("Location Request Denied");
-        $("#sunset").html("Location Request Denied");
-        $("#clouds").html("Location Request Denied");
-        $("#feelsLike").html("Location Request Denied");
-      } else {
-      console.log("Woah")
-      }
-  
+  if (ultLoc == "") {
+    $("#city-name").html("Location Request Denied");
+    $("#sunrise").html("Location Request Denied");
+    $("#sunset").html("Location Request Denied");
+    $("#clouds").html("Location Request Denied");
+    $("#feelsLike").html("Location Request Denied");
+  } else {
+    console.log("Woah")
+  }
+
   //Breaking to the search functionality
 
   var city = ""
 
-      $("#getForecast").click(function(){
-        city = $("#city").val();
+  $("#getForecast").click(function () {
+    city = $("#city").val();
+    $("#currentDay").html(function(){
+      //var daysOfweek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+      var d = new Date();
+      var today = d.getDay();
+      return today;
+    })
 
+    var xhr = new XMLHttpRequest();
+    var weather;
 
-        var xhr = new XMLHttpRequest();
-      var weather;
-  
-      xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            setWeather(JSON.parse(this.responseText));
-        }
-      };
-  
-      xhr.open("GET", "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=metric"+"&appid="+apiKey);
-      xhr.send();
-      
-      function setWeather(jsonWeather) {
-        weather = jsonWeather;
-        console.log(jsonWeather);
-        $("#searchCityName").html((jsonWeather.city.country) + ", " + (jsonWeather.city.name));
-        var sunrise = jsonWeather.city.sunrise;
-        var hum_sunrise = new Date(1000*sunrise)
-        $("#searchSunrise").html(hum_sunrise);
-        var sunset = jsonWeather.city.sunset;
-        var hum_sunset = new Date(1000*sunset)
-        $("#searchSunset").html(hum_sunset);
-        $("#searchClouds").html((jsonWeather.list[0].clouds.all) + "% - " + "\""+ (jsonWeather.list[0].weather[0].description) + "\"");
-        $("#searchFeelsLike").html((jsonWeather.list[0].main.feels_like) + "&deg;C");
-
-        $("#todayPlus1").html(jsonWeather.list[8].dt_txt);
-        $("#searchCityNameTom").html((jsonWeather.city.country) + ", " + (jsonWeather.city.name));
-        $("#searchCloudsTom").html((jsonWeather.list[8].clouds.all) + "% - " + "\""+ (jsonWeather.list[8].weather[0].description) + "\"");
-        $("#searchHumidityTom").html((jsonWeather.list[8].main.humidity) + "&percnt;");
-        $("#searchFeelsLikeTom").html((jsonWeather.list[8].main.feels_like) + "&deg;C");
-  
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        setWeather(JSON.parse(this.responseText));
       }
-        
+    };
 
-      
-      });
-  
-      
+    xhr.open("GET", "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric" + "&appid=" + apiKey);
+    xhr.send();
+
+    function setWeather(jsonWeather) {
+      weather = jsonWeather;
+      console.log(jsonWeather);
+      $("#searchCityName").html((jsonWeather.city.country) + ", " + (jsonWeather.city.name));
+      var sunrise = jsonWeather.city.sunrise;
+      var hum_sunrise = new Date(1000 * sunrise)
+      $("#searchSunrise").html(hum_sunrise);
+      var sunset = jsonWeather.city.sunset;
+      var hum_sunset = new Date(1000 * sunset)
+      $("#searchSunset").html(hum_sunset);
+      $("#timeNowtoday").html(jsonWeather.list[0].dt_txt);
+      $("#searchClouds").html((jsonWeather.list[0].clouds.all) + "% - " + "\"" + (jsonWeather.list[0].weather[0].description) + "\"");
+      $("#cloudsPic").attr("src", `http://openweathermap.org/img/wn/${(jsonWeather.list[0].weather[0].icon)}@2x.png`)
+      $("#searchFeelsLike").html((jsonWeather.list[0].main.feels_like) + "&deg;C");
+      $("#searchTemp").html((jsonWeather.list[0].main.temp) + "&deg;C");
+      $("#searchTempmin").html((jsonWeather.list[0].main.temp_min) + "&deg;C");
+      $("#searchTempmax").html((jsonWeather.list[0].main.temp_max) + "&deg;C");
+
+    }
+  });
 });
 
